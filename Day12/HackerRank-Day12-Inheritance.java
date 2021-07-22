53316e8bf808c06b0a385569c4210c90d371a240
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-07-22 12:06:03 -0700

    Create HackerRank-Day12-Inheritance.java

---
+++ Day12/HackerRank-Day12-Inheritance.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+class Student extends Person{
+	private int[] testScores;
+    
+    public Student(String firstName,String lastName,int idNumber, int[] scores ){
+        super(firstName,lastName,idNumber);
+        this.testScores=scores;
+    }
+    
+    public char calculate(){
+        int average=0;
+        for(int i=0;i<testScores.length;i++){
+            average=average+testScores[i];
+        }
+        average=average/testScores.length;
+        return (average>=90? 'O':average>=80?'E':average>=70?'A':average>=55?'P':average>=40?'D':'T');
+    }
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate() );
+	}
+}
